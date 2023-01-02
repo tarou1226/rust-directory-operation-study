c0688ccc8dcb1694cd8625579e6c985bd48e1d2b
taro <teroro1226@outlook.jp>
2023-01-02 15:01:01 +0900

    feat: show environmental variables

--- src/main.rs
+++ src/main.rs
@@ -15,4 +15,18 @@ fn main() {
     // カレントディレクトリを変更した後の表示
     let young_current = env::current_dir().unwrap();
     println!("young current dir -> {}", young_current.display());
+
+    // 環境変数の取得 （PATH）の取得を行っている
+    let key = "PATH";
+    match env::var_os(key) {
+        Some(paths) => {
+            // 複数の場合は改行しながら、splitで分割、全て表示
+            for path in env::split_paths(&paths) {
+                println!("'{}'", path.display());
+            }
+        }
+        None => println!("{key} is not defined in the environment.")
+    }
+
+    
 }
